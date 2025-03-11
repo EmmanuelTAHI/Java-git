5fed85ddf8034e738e342fd04b8d2ef7e61bc8d1
EmmanuelTAHI <emmanueltahi14@gmail.com>
2025-03-11 14:42:35 +0000

    Second status

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--------------------------------");
+        System.out.println("\tBienvenu dans l'application");
+        System.out.println("--------------------------------\n\n");
+
+        System.out.println("\tConnexion\n\n\n");
+
+        System.out.print("Nom d'utilisateur : ");
+        String nom = s-canner.nextLine();
+
+        System.out.print("Mot de passe : ");
+        String motDePasse = scanner.nextLine();
+
+        Utilisateur user = new Utilisateur(nom, motDePasse);
+
+        if (user.authentification()) {
+            System.out.println("Le mot de passe est correct ✅\n");
+            Menu menu = new Menu();
+            menu.afficherMenu();
+        } else {
+            System.out.println("Le mot de passe est incorrect ❌");
+        }
+
+        scanner.close();
+    }
+}
